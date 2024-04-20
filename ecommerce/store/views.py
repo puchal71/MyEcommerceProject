6bc337191c2f6d3a8c1731149b110b05a8f61966
puchal71 <puchal71@gmail.com>
2024-04-20 17:56:34 +0200

    Added to checkout.html dynamic photo, price, amount of ordered gods and total weight and price.

--- ecommerce/store/views.py
+++ ecommerce/store/views.py
@@ -28,5 +28,15 @@ def cart(request):
 
 
 def checkout(request):
-    context = {}
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        items = order.orderitem_set.all()
+    else:
+        items = []
+
+        # Dodane, zeby przy niezalogowanym uzytkowniku nie wywalalo bleu tylko dawalo pusty koszyk.
+        order = {'get_cart_total': 0, 'get_cart_items': 0}
+
+    context = {'items': items, 'order': order}
     return render(request, 'store/checkout.html', context)
