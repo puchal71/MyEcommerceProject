63f64467884187a639963e1c59179843a0448348
puchal71 <puchal71@gmail.com>
2024-04-20 17:09:05 +0200

    Cart fixing. Added dynamic item name, price and ordered amount.

--- ecommerce/store/views.py
+++ ecommerce/store/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import *
+from .models import User
 
 
 # Create your views here.
@@ -11,7 +12,14 @@ def store(request):
 
 
 def cart(request):
-    context = {}
+
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        items = order.orderitem_set.all()
+    else:
+        items = []
+    context = {'items': items}
     return render(request, 'store/cart.html', context)
 
 
