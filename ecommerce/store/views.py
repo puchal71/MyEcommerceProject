74419744608d769bc4cba6992388d6a9ee95f512
puchal71 <puchal71@gmail.com>
2024-04-21 12:30:54 +0200

    Add to cart button logic added

--- ecommerce/store/views.py
+++ ecommerce/store/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from .models import *
 from .models import User
 from django.http import JsonResponse
-
+import json
 
 # Create your views here.
 
@@ -44,6 +44,29 @@ def checkout(request):
 
 
 def updateItem(request):
+    data = json.loads(request.body)
+    productId = data['productId']
+    action = data['action']
+
+    print('Action:', action)
+    print('productId:', productId)
+
+    customer = request.user.customer
+    product = Product.objects.get(id=productId)
+    order, created = Order.objects.get_or_create(customer=customer, complete=False)
+
+    orderItem, created = OrderItem.objects.get_or_create(order=order, product=product)
+
+    if action == 'add':
+        orderItem.quantity = (orderItem.quantity +100)
+    elif action == 'remove':
+        orderItem.quantity = (orderItem.quantity -100)
+
+    orderItem.save()
+
+    if orderItem.quantity <= 0:
+        orderItem.delete()
+
     return JsonResponse('dodano obiekt', safe=False)
 
 
