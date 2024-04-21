9c499cdcfd98b07d135f150a96a0c1f9503b98d2
puchal71 <puchal71@gmail.com>
2024-04-21 13:36:10 +0200

    Logic added to arrows to change quantity of products in cart.

--- ecommerce/store/views.py
+++ ecommerce/store/views.py
@@ -7,6 +7,21 @@ import json
 # Create your views here.
 
 def store(request):
+
+# Dodane zeby pokazac ilosc produktow w koszyku dla widoku. Nie bede uzywal raczej.
+
+    # if request.user.is_authenticated:
+    #     customer = request.user.customer
+    #     order, created = Order.objects.get_or_create(customer=customer, complete=False)
+    #     items = order.orderitem_set.all()
+    #     cartItems = order.get_cart_items
+    # else:
+    #     items = []
+    #
+    #     # Dodane, zeby przy niezalogowanym uzytkowniku nie wywalalo bleu tylko dawalo pusty koszyk.
+    #     order = {'get_cart_total': 0, 'get_cart_items': 0}
+    #     cartItems = order['get_cart_items']
+
     products = Product.objects.all()
     context = {'products': products}
     return render(request, 'store/store.html', context)
